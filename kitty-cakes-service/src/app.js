601f649dac1c47eca7a3a802bfaa1b19df9827e5
blamelessHellscape <ginganinjarules@gmail.com>
2023-01-28 09:46:45 -0500

    what

--- kitty-cakes-service/src/app.js
+++ kitty-cakes-service/src/app.js
@@ -1,24 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+
 require("dotenv").config({ path: "../config.env" });
-const AccessToken = require("twilio").jwt.AccessToken;
-const ChatGrant = AccessToken.ChatGrant;
-
-//https://www.npmjs.com/package/in-memory-database
-const { InMemoryDatabase } = require("in-memory-database");
-const { target } = require('./target');
-
-const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
-const twilioApiKey = process.env.TWILIO_API_KEY;
-const twilioApiSecret = process.env.TWILIO_API_SECRET;
-
-const serviceSid = process.env.TWILIO_CHAT_SERVICE_SID;
-const identity = "ww.kitty.cakes@gmail.com";
-
-const chatGrant = new ChatGrant({
-  serviceSid: serviceSid,
-});
 
 app.use(
   express.raw({
@@ -27,27 +11,11 @@ app.use(
   })
 );
 
-const db = new InMemoryDatabase();
-db.set("maple", 0);
-db.set("apricot", 0);
-
 app.get("/auth", (req, res) => {
   let secret = getSecret();
-  res.json({ 'secret': secret });
-});
 
-app.post("/donate", (req, res) => {
-  const target_cat = req.body.cat;
-  const amount = req.body.amount;
-  let total = db.get(target_cat);
-  let new_total = total + amount;
-  db.set(target_cat, new_total);
-
-  if (db.get('maple') > db.get('apricot')){
-    target('maple');
-  }else{
-    target('apricot');
-  }
+  res.json({ secret: secret });
+  // res.sendStatus(200)
 });
 
 app.listen(port, () => {
@@ -55,17 +23,8 @@ app.listen(port, () => {
 });
 
 function getSecret() {
-  // const token = new AccessToken(
-  //   twilioAccountSid,
-  //   twilioApiKey,
-  //   twilioApiSecret,
-  //   { identity: identity }
-  // );
-
-  // token.addGrant(chatGrant);
-  // const jwt = token.toJwt();
-  // // Serialize the token to a JWT string
-  // console.log(jwt);
-  return process.env.TWITCH_API
-  //TODO: figure this out
+  // TODO: Get secret from secure storage. This is the secret you pass
+  // when you subscribed to the event.
+  console.log(process.env.TWITCH_SECRET);
+  return process.env.TWITCH_SECRET;
 }
