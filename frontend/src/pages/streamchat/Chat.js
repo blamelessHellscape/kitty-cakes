a0b12678806f9463e25551749a74617000c73a67
Quinnan Gill <quinnan.gill@gmail.com>
2023-01-28 17:03:07 -0500

    Adding stuff

--- frontend/src/pages/streamchat/Chat.js
+++ frontend/src/pages/streamchat/Chat.js
@@ -1,11 +1,124 @@
 import React from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Client as ConversationsClient } from "@twilio/conversations";
 
-function Chat() {
-    return (
-        <div className='chat'>
-            Chat
-        </div>
-    )
+class ChatScreen extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: "",
+            messages: [],
+            loading: false,
+            channel: null,
+        };
+
+        // this.scrollDiv = React.createRef();
+    }
+
+    getToken = async () => {
+        const response = await axios.get(`http://localhost:5000/auth`);
+        const { data } = response;
+        return data.secret;
+    }
+
+    componentDidMount = async () => {
+        const { location } = this.props;
+        const { state } = location || {};
+        const { room } = state || {};
+        let token = {};
+        console.log("---------------------")
+
+        try {
+            token = await this.getToken();
+        } catch {
+            throw new Error("unable to get token, please reload this page");
+        }
+
+        // const {secret, sid} = token;
+        const client = await ConversationsClient(token);
+
+        client.on("tokenAboutToExpire", async () => {
+            const token = await this.getToken();
+            client.updateToken(token);
+        });
+
+        client.on("tokenExpired", async () => {
+            const token = await this.getToken();
+            client.updateToken(token);
+        });
+
+        client.on("channelJoined", async (channel) => {
+            // getting list of all messages since this is an existing channel
+            const messages = await channel.getMessages();
+            this.setState({ messages: messages.items || [] });
+            this.scrollToBottom();
+        });
+
+        try {
+            const channel = await client.getChannelByUniqueName(room);
+            await this.joinChannel(channel);
+            this.setState({ channel, loading: false })
+        } catch {
+            try {
+                const channel = await client.createChannel({
+                    uniqueName: room,
+                    friendlyName: room,
+                });
+                await this.joinChannel(channel);
+            } catch {
+                throw new Error("unable to create channel, please reload this page");
+            }
+        }
+    };
+
+    joinChannel = async (channel) => {
+        if (channel.channelState.status !== "joined") {
+            await channel.join();
+        }
+        channel.on("messageAdded", this.handleMessageAdded);
+    };
+
+    handleMessageAdded = (message) => {
+        const { messages } = this.state;
+        this.setState(
+            {
+                messages: !!messages ? [...messages, message] : [message],
+            },
+            this.scrollToBottom
+        );
+    };
+
+    scrollToBottom = () => {
+        console.log("Scroll to bottom");
+    };
+
+    sendMessage = () => {
+        const { text, channel } = this.state;
+        if (text && String(text).trim()) {
+            this.setState({ loading: true });
+            channel && channel.sendMessage(text);
+            this.setState({ text: "", loading: false })
+        }
+    };
+
+    render() {
+        const { loading, text, messages, channel } = this.state;
+        const { location } = this.props;
+
+        return (
+            <div className='chat-window'>
+                {/* <Form>
+                    <Form.Group className='message' controlId="formMessage">
+                        
+                    </Form.Group>
+                </Form> */}
+                Chat
+            </div>
+        )
+    }
 }
 
-export default Chat;
+export default ChatScreen;
--- kitty-cakes-service-flask/src/app.py
+++ kitty-cakes-service-flask/src/app.py
@@ -1,15 +1,17 @@
-from flask import Flask, jsonify, request, Response
+from flask import Flask, jsonify
+from flask_cors import CORS
 import os
 from dotenv import load_dotenv
+import requests
 from target import target
 import sys
 
 app = Flask(__name__)
+CORS(app)
 load_dotenv()
 
 db = {'maple': 0,
       'apricot': 0}
-
 
 @app.route("/")
 def hello():
@@ -20,6 +22,16 @@ def hello():
 def auth():
     return jsonify({"secret": os.getenv("TWILIO_AUTH_TOKEN"), "sid": os.getenv("TWILIO_ACCOUNT_SID")})
 
+@app.route("/conversation", methods=["GET"])
+def get_conversation():
+    global conversation
+    if not conversation:
+        account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+        auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+
+        return jsonify({"conversation": conversation.sid})
+    else:
+        return jsonify({"conversation": conversation.sid})
 
 @app.route("/donate", methods=["POST"])
 def donate():
