70566b35383cb51032bd8d53a7e06fae1ef916a7
blamelessHellscape <ginganinjarules@gmail.com>
2023-01-28 19:23:40 -0500

    restructure!

--- kitty-cakes-controller/target.py
+++ kitty-cakes-controller/target.py
@@ -1,16 +1,16 @@
-# from flask import Response
-import json, requests
+from flask import make_response
+import requests
 
 pi_addr = "http://192.168.50.150:80/pussycat"; #insert actual IP here.
 def target(cat):
-    data = {'is_in_frame': False}
+    data = 'flag=',False
     #contact GCP here and figure out if cat is in frame. 
     #return true or false if cat is in frame. motor will move until true 
-    headers = {"Content-type": "application/json"}
+    # headers = {"Content-type": "application/json"}
     
     try:
-        response = requests.post(pi_addr, data=json.dumps(data), headers=headers)
-        return ('good',response.status_code)
+        response = requests.post(pi_addr, data=data)
+        return make_response('good',response.status_code)
     except Exception:
         print('beef')
         return ('beef1',501)
